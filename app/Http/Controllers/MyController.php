8fb94e888c1f21f70c954c5cb4f400848854f48d
Brandon <btoney434@gmail.com>
2019-11-22 01:09:21 -0500

    fix region filter

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -226,8 +226,9 @@ class MyController extends Controller
             */
             ->when(count($request->required_keywords), function ($query) use ($regionParams, $keywordParams) {
                 $query->when(count($regionParams), function ($q) use ($regionParams) {
-                    if (count($regionParams) == 1) {
-                        $region = $regionParams[0];
+                    if (count($regionParams) == 1 || count($regionParams) == 2) {
+                        // $region = $regionParams[0];
+                        /*
                         $q->where(function ($q1) use ($region) {
                             $q1->where('decks.region1', $region)
                                 ->whereNull('decks.region2');
@@ -235,8 +236,11 @@ class MyController extends Controller
                             $q2->whereNull('decks.region1')
                                 ->where('decks.region2',$region);
                         });
-                    } 
-                    else if (count($regionParams) == 2) {
+                        */
+                        //$q->whereIn('decks.region1', $region)
+                        //    ->orWhereIn('decks.region2', $region);
+                    //} 
+                    //else if (count($regionParams) == 2) {
                         $q->whereIn('decks.region1', $regionParams)
                             ->whereIn('decks.region2', $regionParams);
                     } else {
