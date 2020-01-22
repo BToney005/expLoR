074edce14e26fd22ddda6aee967f6a28ecf4980a
rachd <rachel.m.dorn@gmail.com>
2020-01-21 19:06:07 -0500

    set quintile boundaries

--- app/Http/Controllers/DeckController.php
+++ app/Http/Controllers/DeckController.php
@@ -27,4 +27,24 @@ class DeckController extends Controller
 
         return response()->json(['top_decks' => $top_decks, 'message' => 'DECKS FOUND'], 200);
     }
+
+    public function setRanks() {
+        $sorted_decks = MATCH::select(DB::raw('deck_code, sum(result) * sum(result) / count(result) as score'))
+            ->whereBetween('created_at', [Carbon::now()->subMonths(1), Carbon::now()])
+            ->groupBy('deck_code')
+            ->get();
+        $quintile_boundaries = array_chunk($sorted_decks, 5)
+            ->map(function($ntile) {
+                return end($ntile)
+            })
+        $quintiles = array(
+            array('rank'=>'S', 'lower_bound'=> $quintile_boundaries[0]),
+            array('rank'=>'A', 'lower_bound'=> $quintile_boundaries[1]),
+            array('rank'=>'B', 'lower_bound'=> $quintile_boundaries[2]),
+            array('rank'=>'C', 'lower_bound'=> $quintile_boundaries[3]),
+        );
+        DB::table('ranks')->delete();
+        DB::table('ranks')->insert($data);
+        return response()->json(['message' => 'QUINTILES SET'], 200);
+    }
 }
